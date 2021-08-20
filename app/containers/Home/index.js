858160d39390e3277213fd2293e1d700c32f0b25
Deepanshu Prajapati <dipanshuraz2@gmail.com>
2021-08-20 22:46:10 +0530

    Initial commit

---
+++ app/containers/Home/index.js
@@ -0,0 +1,45 @@
+/**
+ *
+ * Home
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { compose } from 'redux';
+
+import { useInjectSaga } from 'utils/injectSaga';
+import { useInjectReducer } from 'utils/injectReducer';
+import makeSelectHome from './selectors';
+import reducer from './reducer';
+import saga from './saga';
+
+export function Home() {
+  useInjectReducer({ key: 'home', reducer });
+  useInjectSaga({ key: 'home', saga });
+
+  return <div />;
+}
+
+Home.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = createStructuredSelector({
+  home: makeSelectHome(),
+});
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+
+export default compose(withConnect)(Home);
